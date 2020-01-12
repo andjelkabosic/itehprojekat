df0ec6ecc19c4caac07f1a62e4b0401274d54426
andjelkabosic <andjaboss@gmail.com>
2020-01-13 00:02:31 +0100

    obrisano

--- footer.php
+++
@@ -1,3 +0,0 @@
-
-
-<footer class="text-center" id="footer"><br>&copy; Copyright 2020 AJI Zalihe by AndjaJokaIgi</footer>
--- obrisiStanje.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('konekcija.php');
-$id = $_GET['id'];
-$db-> where("stanjeID",$id);
-$db -> delete('stanjezaliha');
-header("Location: trenutnoStanje.php");
- ?>
